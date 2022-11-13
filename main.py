73c02d1fdd08daa8697b5b4b68e544e5e922d876
Joris Masson <joris.masson2002@gmail.com>
2022-11-13 22:11:43 +0100

    nettoyage de l'ancien main

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ from discord.ext import commands
 from discord_components import DiscordComponents
 from dotenv import load_dotenv
 from utils.functions import log
-from utils.func import detect_message, delete_if_not_noice_image
-from utils.classes.recherche import Recherche
-from utils.classes.ledrgb import LedRgb
-
-# données additionnelles
-from data.dico_quest_books import dico_quest_books
-from data.dico_books import dico_books
-from data.dico_artifacts import dico_artifacts
 
 # imports des commandes
 from commands import*
@@ -30,26 +22,21 @@ TOKEN = os.getenv("TOKEN")  # charge le token
 
 connected = False  # si le bot est connecté, pour éviter que les logs fassent n'importe quoi
 
-ma_led = LedRgb(16, 20, 26)  # définition de la led
-
 
 # ----- events -----
 @kazooha.event
 async def on_connect():
     global connected
-    ma_led.set_color("jaune")
     connected = True
     log(f"{kazooha.user.name} est connecté!")
 
 
 @kazooha.event
 async def on_ready():
-    ma_led.set_color("vert")
     for guild in kazooha.guilds:
         log(f"{kazooha.user.name} est prêt dans {guild.name}({guild.id})!")
 
     await kazooha.change_presence(status=discord.Status.online, activity=discord.Game(f"vous donner des infos sur le jeu"))  # Définit le jeu du bot
-    ma_led.stop()
 
 
 @kazooha.event
@@ -58,17 +45,6 @@ async def on_disconnect():
     if connected:
         log(f"{kazooha.user.name} a été déconnecté de Discord")
         connected = False
-
-
-@kazooha.event
-async def on_message(msg: discord.Message):
-    global kazooha
-    ma_led.set_color("cyan")
-    # await delete_if_not_noice_image(msg)
-    await detect_message(msg)
-    await Recherche(msg, kazooha, ma_led).reply_with_sauce()
-    await kazooha.process_commands(msg)
-    ma_led.stop()
 
 # ----- commandes -----
 kazooha.add_cog(ServerStat(kazooha))
