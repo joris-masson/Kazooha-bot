29a81c3c6d9a4ed2aded4a439a665aa6435f19a1
Joris Masson <joris.masson2002@gmail.com>
2022-11-13 22:09:31 +0100

    finition de la migration vers interactions.py, refonte des logs

--- main_slash.py
+++ main_slash.py
@@ -3,13 +3,18 @@ import interactions
 
 from dotenv import load_dotenv
 from discord_components import DiscordComponents
+from utils.func import detect_message
+from utils.classes.recherche import Recherche
+from utils.classes.ledrgb import LedRgb
 
 
 load_dotenv()  # prépare le chargement du token
 TOKEN = os.getenv("TOKEN")  # charge le token
 
-kazooha = interactions.Client(token=TOKEN)
+kazooha = interactions.Client(token=TOKEN, intents=interactions.Intents.ALL)
 DiscordComponents(kazooha)
+
+ma_led = LedRgb(16, 20, 26)  # définition de la led
 
 kazooha.load('interactions.ext.files')
 
@@ -19,6 +24,14 @@ kazooha.load("commands.slash.showquestbooks")
 kazooha.load("commands.slash.showartifacts")
 kazooha.load("commands.slash.showcollection")
 kazooha.load("commands.slash.help")
+
+
+@kazooha.event
+async def on_message_create(msg: interactions.Message):
+    ma_led.set_color("cyan")
+    await detect_message(msg)
+    # await Recherche(msg, kazooha, ma_led).reply_with_sauce()
+    ma_led.stop()
 
 
 @kazooha.command(
--- utils/func.py
+++ utils/func.py
@@ -1,30 +1,33 @@
 import datetime
 import time
 
+import interactions
 import discord
 import os
 
 from utils.functions import log
 
 
-async def detect_message(msg: discord.Message) -> None:
+async def detect_message(msg: interactions.Message) -> None:
     date = datetime.datetime.now().strftime("%d-%m-%Y")
     if not msg.author.bot and not len(msg.content) == 0:
-        time = datetime.datetime.now().strftime("%H:%M:%S")
+        guild = await msg.get_guild()
+        channel = await msg.get_channel()
+        ze_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-        ze_log = f"[{time}] - {msg.author.name} a dit: {msg.content}\n"
+        ze_log = f"[{ze_time}] - {msg.author.username} a dit: {msg.content}\n"
         print(f"[MSG] - {ze_log}", end='')
         try:
-            path = f"message_logs/{date}/{msg.guild.name}/{msg.channel.name}.txt"
+            path = f"message_logs/{date}/{guild.name}/{channel.name}.txt"
         except AttributeError:
-            path = f"message_logs/{date}/{msg.author.name}.txt"
+            path = f"message_logs/{date}/{msg.author.username}.txt"
         await image_log(msg, date)
         try:
             try:
                 with open(path, 'a') as log_file:
                     log_file.write(ze_log)
             except FileNotFoundError:
-                os.makedirs(rf"message_logs/{date}/{msg.guild.name}")
+                os.makedirs(rf"message_logs/{date}/{guild.name}")
                 with open(path, 'a') as log_file:
                     log_file.write(ze_log)
         except UnicodeEncodeError:
@@ -32,7 +35,7 @@ async def detect_message(msg: discord.Message) -> None:
                 with open(path, 'a', encoding='utf-8') as log_file:
                     log_file.write(ze_log)
             except FileNotFoundError:
-                os.makedirs(rf"message_logs/{date}/{msg.guild.name}")
+                os.makedirs(rf"message_logs/{date}/{guild.name}")
                 with open(path, 'a', encoding='utf-8') as log_file:
                     log_file.write(ze_log)
     elif not msg.author.bot:
