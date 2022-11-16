c5bfc02d785d166f03460ede03a608235a480c1f
Joris Masson <joris.masson2002@gmail.com>
2022-11-17 00:28:06 +0100

    Rajout des descriptions des commandes dans le /help

--- commands/slash/help.py
+++ commands/slash/help.py
@@ -24,6 +24,7 @@ class Help(interactions.Extension):
                 **Nom de la commande**: `/afficher_les_artefacts`
                 **Paramètre**: `page`
                 Valeurs possibles pour `page`: `1` ou `2`
+                **Description**: affiche une liste des artéfacts du jeu, et vous permet de lire l'histoire de toutes les pièces du set. 
                 
                 **Visibilité**: *cette commande est visible par les autres*
 
@@ -34,6 +35,7 @@ class Help(interactions.Extension):
                 **Nom de la commande**: `/afficher_les_livres_des_archives`
                 **Paramètre**: `page`
                 Valeurs possibles pour `page`: `1` ou `2`
+                **Description**: affiche une liste des livres disponibles dans les archives du jeu, et vous permet de les lire.
                 
                 **Visibilité**: *cette commande est visible par les autres*
 
@@ -42,6 +44,7 @@ class Help(interactions.Extension):
 
         help_embed.add_field(name="__**Afficher les livres de quêtes**__", value="""                
                 **Nom de la commande**: `/afficher_les_livres_de_quete`
+                **Description**: affiche une liste des livres de quêtes, et vous permet de les lire.
 
                 **Visibilité**: *cette commande est visible par les autres*
 
@@ -50,6 +53,7 @@ class Help(interactions.Extension):
 
         help_embed.add_field(name="__**Afficher un dossier confidentiel**__", value="""
                 **Nom de la commande**: `/dossiers_confidentiels`
+                **Description**: affiche une liste des dossiers confidentiels, des résumés en une image du lore autour d'un personnage. Ils sont tous faits par <@783075596280004659>.
 
                 **Visibilité**: *cette commande n'est pas visible par les autres*
 
@@ -58,6 +62,7 @@ class Help(interactions.Extension):
 
         help_embed.add_field(name="__**Afficher les personnages dont les aptitudes sont farmables aujourd'hui**__", value="""
                         **Nom de la commande**: `/afficher_persos_farmables`
+                        **Description**: Affiche en une image, tous les personnages dont les matériaux d'aptitudes sont disponibles aujourd'hui.
 
                         **Visibilité**: *cette commande n'est pas visible par les autres*
 
@@ -67,6 +72,8 @@ class Help(interactions.Extension):
         help_embed.add_field(name="__**Afficher les armes farmables aujourd'hui**__",
                              value="""
                                 **Nom de la commande**: `/afficher_armes_farmables`
+                                **Description**: Affiche en une image, tous les matériaux d'élévation d'arme disponibles aujourd'hui.
+
 
                                 **Visibilité**: *cette commande n'est pas visible par les autres*
 
