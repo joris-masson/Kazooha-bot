ef964c906ec3bab1a65e19baae779687d39eed36
Joris Masson <joris.masson2002@gmail.com>
2022-11-17 00:13:54 +0100

    Refonte du /help

--- commands/slash/help.py
+++ commands/slash/help.py
@@ -21,44 +21,56 @@ class Help(interactions.Extension):
         help_embed.add_field(name="__**Préfix**__", value="Le bot utilise maintenant les commandes slashs!\ncommencez votre commande par `/` et vous devriez être guidés!")
 
         help_embed.add_field(name="__**Afficher les artéfacts**__", value="""
-                Nom de la commande: `/afficher_les_artefacts`
-                Paramètre: `page`
+                **Nom de la commande**: `/afficher_les_artefacts`
+                **Paramètre**: `page`
                 Valeurs possibles pour `page`: `1` ou `2`
+                
+                **Visibilité**: *cette commande est visible par les autres*
 
-                Exemples: `/afficher_les_artefacts 1` | `/afficher_les_artefacts 2`
+                **Exemples**: `/afficher_les_artefacts 1` | `/afficher_les_artefacts 2`
                 """, inline=False)
 
         help_embed.add_field(name="__**Afficher les livres des archives**__", value="""
-                Nom de la commande: `/afficher_les_livres_des_archives`
-                Paramètre: `page`
+                **Nom de la commande**: `/afficher_les_livres_des_archives`
+                **Paramètre**: `page`
                 Valeurs possibles pour `page`: `1` ou `2`
+                
+                **Visibilité**: *cette commande est visible par les autres*
 
-                Exemples: `/afficher_les_livres_des_archives 1` | `/afficher_les_livres_des_archives 2`
+                **Exemples**: `/afficher_les_livres_des_archives 1` | `/afficher_les_livres_des_archives 2`
                 """, inline=False)
 
-        help_embed.add_field(name="__**Afficher les livres de quêtes**__", value="""
-                Nom de la commande: `/afficher_les_livres_de_quete`
+        help_embed.add_field(name="__**Afficher les livres de quêtes**__", value="""                
+                **Nom de la commande**: `/afficher_les_livres_de_quete`
 
-                Exemple: `/afficher_les_livres_de_quete`
+                **Visibilité**: *cette commande est visible par les autres*
+
+                **Exemple**: `/afficher_les_livres_de_quete`
                 """, inline=False)
 
         help_embed.add_field(name="__**Afficher un dossier confidentiel**__", value="""
-                Nom de la commande: `/dossiers_confidentiels`
+                **Nom de la commande**: `/dossiers_confidentiels`
 
-                Exemple: `/dossiers_confidentiels`
+                **Visibilité**: *cette commande n'est pas visible par les autres*
+
+                **Exemple**: `/dossiers_confidentiels`
                 """, inline=False)
 
         help_embed.add_field(name="__**Afficher les personnages dont les aptitudes sont farmables aujourd'hui**__", value="""
-                        Nom de la commande: `/afficher_persos_farmables`
+                        **Nom de la commande**: `/afficher_persos_farmables`
 
-                        Exemple: `/afficher_persos_farmables`
+                        **Visibilité**: *cette commande n'est pas visible par les autres*
+
+                        **Exemple**: `/afficher_persos_farmables`
                         """, inline=False)
 
         help_embed.add_field(name="__**Afficher les armes farmables aujourd'hui**__",
                              value="""
-                                Nom de la commande: `/afficher_armes_farmables`
+                                **Nom de la commande**: `/afficher_armes_farmables`
 
-                                Exemple: `/afficher_armes_farmables`
+                                **Visibilité**: *cette commande n'est pas visible par les autres*
+
+                                **Exemple**: `/afficher_armes_farmables`
                                 """, inline=False)
 
         help_embed.set_thumbnail(url=self.client.me.icon_url)
