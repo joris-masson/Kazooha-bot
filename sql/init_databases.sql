a54bdbfe073b969fa955c16052144381d70db87c
jo <joris.masson2002@gmail.com>
2024-04-21 17:48:29 +0200

    Script initialisation des tables de la base de données

--- sql/init_databases.sql
+++ sql/init_databases.sql
@@ -2,26 +2,52 @@ CREATE DATABASE IF NOT EXISTS Kazooha;
 SET NAMES utf8mb4;
 ALTER DATABASE Kazooha CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS Kazooha.Messages (
-    id BIGINT NOT NULL PRIMARY KEY,
-    guildId BIGINT NOT NULL,
-    guildName VARCHAR(100) NOT NULL,
-    channelId BIGINT NOT NULL,
-    channelName VARCHAR(100) NOT NULL,
-    authorId BIGINT NOT NULL,
-    authorName VARCHAR(100) NOT NULL,
-    sentTime TIMESTAMP NOT NULL,
-    content VARCHAR(4000) NOT NULL,
-    modified BOOLEAN,
-    deleted BOOLEAN
+# DROP TABLE IF EXISTS Kazooha.GameUid;
+CREATE TABLE IF NOT EXISTS Kazooha.GameUid
+(
+    discordId BIGINT       NOT NULL,
+    game      VARCHAR(100) NOT NULL,
+    server    VARCHAR(100),
+    uid       BIGINT       NOT NULL PRIMARY KEY,
+    nickname  VARCHAR(255),
+    level     INT
 );
 
-DROP TABLE Kazooha.GameUid;
-CREATE TABLE IF NOT EXISTS Kazooha.GameUid (
-    discordId BIGINT NOT NULL,
-    game VARCHAR(100) NOT NULL,
-    server VARCHAR(100),
-    uid BIGINT NOT NULL PRIMARY KEY,
-    nickname VARCHAR(255),
-    level INT
+# DROP TABLE IF EXISTS Kazooha.Character;
+CREATE TABLE IF NOT EXISTS Kazooha.Character
+(
+    id            INT(20)     NOT NULL PRIMARY KEY,
+    name          VARCHAR(50) NOT NULL,
+    rarity        INT(1)      NOT NULL,
+    element       VARCHAR(25) NOT NULL,
+    weapon        VARCHAR(25) NOT NULL,
+    birthday      VARCHAR(25) NOT NULL,
+    constellation VARCHAR(50) NOT NULL,
+    affiliation   VARCHAR(100),
+    description   VARCHAR(500),
+    region        VARCHAR(25) NOT NULL,
+    material      VARCHAR(50) NOT NULL,
+    beta          BOOLEAN     NOT NULL DEFAULT 1
+);
+
+# DROP TABLE IF EXISTS Kazooha.Weapon;
+CREATE TABLE IF NOT EXISTS Kazooha.Weapon
+(
+    id INT(20) NOT NULL PRIMARY KEY,
+    name        VARCHAR(50)  NOT NULL,
+    rarity      INT(1)       NOT NULL,
+    type        VARCHAR(25)  NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    story       LONGTEXT     NOT NULL,
+    material    VARCHAR(50)  NOT NULL,
+    beta        BOOLEAN      NOT NULL DEFAULT 1
+);
+
+# DROP TABLE IF EXISTS Kazooha.Material;
+CREATE TABLE IF NOT EXISTS Kazooha.Material
+(
+    name    VARCHAR(50) NOT NULL PRIMARY KEY,
+    utility VARCHAR(20) NOT NULL,
+    region  VARCHAR(25) NOT NULL,
+    day     VARCHAR(25)
 );
